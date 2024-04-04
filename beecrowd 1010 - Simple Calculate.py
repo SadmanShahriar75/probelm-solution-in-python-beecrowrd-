020ab6b71b1dcc2409c5425943aa57f6cae816c9
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-04-04 16:20:41 +0600

    Add new files

---
+++ beecrowd 1010 - Simple Calculate.py
@@ -0,0 +1,4 @@
+code1, units1, price1 = map(float, input().split())
+code2, units2, price2 = map(float, input().split())
+total_price = (units1 * price1) + (units2 * price2)
+print("VALOR A PAGAR: R$ {:.2f}".format(total_price))
