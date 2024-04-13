f111d4811ea1d16db04876bb79b46dc28a73d15c
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-04-14 03:11:23 +0600

    happy coding

---
+++ beecrowd 1038 - Snack.py
@@ -0,0 +1,5 @@
+
+x, y = map(int,(input().split()))
+prices =[0,4.00, 4.50, 5.00, 2.00, 1.50]
+pay = prices[x]*y
+print(f"Total: R$ {pay:.2f}")
---
+++ beecrowd 1042 -  Simple Sort.py
@@ -0,0 +1,14 @@
+a, b, c = map(int, input().split())
+max = [a, b, c]
+max.sort()
+print(f"{max[0]}\n{max[1]}\n{max[2]}\n")
+print(f"{a}\n{b}\n{c}")
+
+
+
+
+
+
+
+
+
