654734667b1bbcff2a3495993da9e5cdc0951d4b
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-05-04 21:50:40 +0600

    happy coding

---
+++ beecrowd 1143 - Squared and Cubic.py
@@ -0,0 +1,5 @@
+n = int(input())
+s = 1
+for i in range(n):
+   print(f"{s} {s**2} {s**3}")
+   s = s + 1
---
+++ beecrowd 1144 - Logical Sequence.py
@@ -0,0 +1,8 @@
+n = int(input())
+s = 1
+k =1
+for i in range(n):
+   print(f"{s} {s**2} {s**3}")
+   print(f"{k} {k**2+1} {k**3+1}")
+   s = s + 1
+   k = k + 1
