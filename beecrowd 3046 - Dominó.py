8ec500eea341ab1126bc4489b00e441f61bf3243
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-06-11 10:41:16 +0600

    life is all about next steps

--- beecrowd 3046 - Dominó.py
+++ beecrowd 3046 - Dominó.py
@@ -1,4 +1,3 @@
-
 n = int(input())
 s = 0
 for i in range(1, n+2):
---
+++ beecrowd 3047 - A idade de Dona Mônica.py
@@ -0,0 +1,12 @@
+
+
+monica = int(input())
+a = int(input())
+b = int(input())
+twoson = a + b
+c = monica - twoson
+oldson = max(a,b,c)
+print(oldson)
+
+
+
